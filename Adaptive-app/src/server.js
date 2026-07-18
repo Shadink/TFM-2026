a45da31a980f0602898f5297aa1a2433b777c662
M VR <taiogao@gmail.com>
2026-07-18 11:21:19 +0200

    Adaptive app se inicia en catalog

--- Adaptive-app/src/server.js
+++ Adaptive-app/src/server.js
@@ -4,6 +4,10 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'catalog.html'));
+});
+
 app.use(express.static('public'));
 
 app.listen(PORT, () => {
