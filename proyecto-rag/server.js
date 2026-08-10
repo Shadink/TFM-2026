989a7d48905e839369f4b5f8eab6ec8f40cd4e38
M VR <taiogao@gmail.com>
2026-08-10 16:54:09 +0200

    Cambiado modelo por terra

--- proyecto-rag/server.js
+++ proyecto-rag/server.js
@@ -7,7 +7,7 @@ import cors from "cors"
 import { randomUUID } from "crypto";
 
 const endpoint = "https://tfm-mvr2026-foundry.services.ai.azure.com/openai/v1";
-const deploymentName = "gpt-5.6-sol";
+const deploymentName = "gpt-5.6-terra";
 
 //import { GoogleGenerativeAI } from "@google/generative-ai";
 
